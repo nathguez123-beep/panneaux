9c8f030fe1e34b37ec08e3f3e4053869d5976210
nathguez123-beep <nathguez123@gmail.com>
2026-08-13 01:40:21 +0300

    Configurer le domaine de production paneaux.netlify.app

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,6 @@ import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://example.fr',
+  site: 'https://paneaux.netlify.app',
   integrations: [sitemap()]
 });
